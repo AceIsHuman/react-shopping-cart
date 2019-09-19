556d880643015a33d81dfdd86f12c71ffb9db97f
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-09-18 21:36:06 -0500

    wrap app in CartContext Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+
 import { ProductContext } from './context/ProductContext';
+import { CartContext } from './context/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
+
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
@@ -18,21 +21,23 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<ProductContext.Provider value={{products, addItem}}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					component={ShoppingCart}
-				/>
-			</ProductContext.Provider>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
